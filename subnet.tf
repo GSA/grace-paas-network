b6318bf521098173ffd1bcd388baeb534a931be5
EdRyan <edward.ryan@gsa.gov>
2020-04-22 10:06:49 -0400

    updated length function

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "self" {
-  count             = len(var.vpc_cidrblocks) * 2
+  count             = length(var.vpc_cidrblocks) * 2
   vpc_id            = aws_vpc.self[floor(count.index / 2)].id
   cidr_block        = cidrsubnet(aws_vpc.self[floor(count.index / 2)].cidr_block, 2, count.index % 2)
   availability_zone = var.availability_zones[count.index % 2]
