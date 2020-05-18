50d767122dc42209cbe8032af9d7a46301ad3c4a
juanpgomez-gsa <juan.gomez@gsa.gov>
2020-05-18 11:57:51 -0400

    Created elb.tf and updated variables file with alb variable

---
+++ elb.tf
@@ -0,0 +1,34 @@
+resource "aws_lb" "app_waf_alb" {
+  name     = "front-end-alb"
+  internal = true
+  security_groups = [
+    "sg-"
+  ]
+  subnets = [
+    aws_subnet.self[0].name,
+    aws_subnet.self[1].name
+  ]
+  ip_address_type    = "ipv4"
+  load_balancer_type = "application"
+}
+
+resource "aws_lb_target_group" "waf_alb" {
+  name     = "alb-frontend-tg"
+  port     = 443
+  protocol = "HTTPS"
+  vpc_id   = aws_vpc.self[0].id
+}
+
+resource "aws_lb_listener" "front_end" {
+  count             = var.alb_cert_arn ? 1 : 0
+  load_balancer_arn = aws_lb.app_waf_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.alb_cert_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.waf_alb
+  }
+}
