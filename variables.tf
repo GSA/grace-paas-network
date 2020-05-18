acf898bebf97c2fb6f36eb243211351948169be6
juanpgomez-gsa <juan.gomez@gsa.gov>
2020-05-18 11:59:56 -0400

    Updated variables file to support elb.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,9 @@ variable "tgw_id" {
   description = "(optional) ID of the Transit Gateway"
   default     = "tgw-0a5a3a1935972e4f0"
 }
+
+variable "alb_cert_arn" {
+  type        = string
+  description = "(optional) ID of certificate in IAM role"
+  default     = null
+}
