2ff6af741d1a24cd204d343e0738b622e6ce4569
juanpgomez-gsa <juan.gomez@gsa.gov>
2020-06-09 16:08:01 -0400

    Code for WAF

---
+++ waf.tf
@@ -0,0 +1,15 @@
+module "waf" {
+  source  = "umotif-public/waf-webaclv2/aws"
+  version = "0.1.0"
+
+  name_prefix = "grace-app"
+  alb_arn     = aws_lb.app_waf_alb.elb_arn
+
+  enable_CommonRuleSet          = true
+  enable_AdminProtectionRuleSet = true
+  enable_AmazonIpReputationList = true
+  enable_AnonymousIpList        = true
+  enable_SQLiRuleSet            = true
+
+
+}
