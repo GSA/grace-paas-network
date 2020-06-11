99cbb48b72a1e397ef608bb746d43f3cba1c0336
Bryan Alexander <bryan.alexander@gsa.gov>
2020-06-11 15:43:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,12 @@ data "aws_ec2_transit_gateway" "tgw" {
 data "aws_organizations_organization" "org" {}
 
 data "aws_route53_resolver_rule" "internal" {
+  count = var.is_hub ? 0 : 1
   domain_name = "${var.internal_domain}"
   rule_type   = "FORWARD"
 }
 
 data "aws_route53_resolver_rule" "external" {
+  count = var.is_hub ? 0 : 1
   name = "forward-outbound"
 }
