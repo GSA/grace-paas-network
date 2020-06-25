868f464991e21f4f4ed877518bc5b441e25e0ef7
Brian Fitzwater - IDI-C <brian.fitzwater@gsa.gov>
2020-06-25 08:53:50 -0500

    WIP

--- iam.tf
+++ iam.tf
@@ -1,5 +1,6 @@
 # Role for grace-paas-associate-zone Lambda function
 resource "aws_iam_role" "lambda_role" {
+  count       = var.is_hub ? 1 : 0
   name        = local.lambda_name
   description = "Role for GRACE PaaS Associate Zone Lambda function"
 
@@ -22,6 +23,7 @@ EOF
 }
 
 resource "aws_iam_policy" "lambda_policy" {
+  count       = var.is_hub ? 1 : 0
   name        = local.lambda_name
   description = "Policy to allow associating VPC to hosted zone"
 
@@ -38,7 +40,7 @@ resource "aws_iam_policy" "lambda_policy" {
       ],
       "Effect": "Allow",
       "Resource": "*"
-    },
+    }
   ]
 }
 EOF
@@ -46,6 +48,7 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_role_policy_attachment" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_policy.arn
+  count      = var.is_hub ? 1 : 0
+  role       = aws_iam_role.lambda_role[0].name
+  policy_arn = aws_iam_policy.lambda_policy[0].arn
 }
--- lambda.tf
+++ lambda.tf
@@ -1,6 +1,10 @@
 resource "aws_lambda_function" "associate" {
-  filename      = var.lambda_source_file
-  function_name = local.lambda_name
-  description   = "Associates tenant VPC with Route53 hosted zone when triggered by Cloudwatch event"
-  role          = aws_iam_role.lambda_role.arn
+  count            = var.is_hub ? 1 : 0
+  filename         = var.lambda_source_file
+  function_name    = local.lambda_name
+  description      = "Associates tenant VPC with Route53 hosted zone when triggered by Cloudwatch event"
+  role             = aws_iam_role.lambda_role[0].arn
+  handler          = "grace-inventory-lambda"
+  source_code_hash = filesha256(var.lambda_source_file)
+  runtime          = "go1.x"
 }
