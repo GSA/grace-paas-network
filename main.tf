b8a9143bb8b5e00bdb7816b1f4ad638d2a19df2f
EdRyan <edward.ryan@gsa.gov>
2020-08-26 15:13:31 -0400

    update project_domain local var with an env letter identifier for test and dev

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 locals {
   // if we're the hub then we're using internal_domain, otherwise prepend the customer project name
-  project_domain = var.is_hub ? var.internal_domain : "${var.project_name}.${var.internal_domain}"
+  env_id         = var.appenv == "production" ? "" : "-" + substr(var.appenv, 0, 1)
+  project_domain = var.is_hub ? var.internal_domain : "${var.project_name}${local.env_id}.${var.internal_domain}"
   lambda_name    = "grace-paas-associate-zone"
   put_events     = "put-events"
   zone_id        = var.is_hub ? aws_route53_zone.hub_internal[0].id : aws_route53_zone.spoke_internal[0].id
